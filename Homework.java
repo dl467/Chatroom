d51fd3336794b0b2df3ae817fce629cbfca0cbd7
dl467 <70830819+dl467@users.noreply.github.com>
2020-09-14 13:49:23 -0400

    Add files via upload
    Java Data Homework

---
+++ Homework.java
@@ -0,0 +1,13 @@
+public class Homework {
+   public static void main(String[] args) {
+   String name = "Paul";
+   System.out.println(name);
+   
+   int age = 19;
+   System.out.println(age);
+   
+   double decimal = 4.9;
+   System.out.println(decimal);
+   }
+}
+   
